553481c9944982aa81f855572b9772b1c459d57b
ericsalat <ericsalat@protonmail.com>
2023-04-25 18:13:12 +0200

    Creación del proyecto

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return(
+        <div class="header">
+            <header>
+            <p>Hi Anakin</p>  
+            </header>
+            <i class="k-icon-bell"></i>
+        </div>
+    );
+}
+
+export default Header;
