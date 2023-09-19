b186099e7edb45d81075011e0c2e1b517fd959f4
Èric Salat <ericsalat@protonmail.com>
2023-09-19 23:45:02 +0200

    Simplificada la lógica de renderizado de los botones del NavMenu

--- src/Components/NavMenu/NavMenu.jsx
+++ src/Components/NavMenu/NavMenu.jsx
@@ -1,61 +1,31 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import "./NavMenu.css";
 import NavButton from "../NavButton/NavButton";
 import ButtonShowTransactions from "../ButtonShowTransactions/ButtonShowTransactions";
 
 function NavMenu() {
+    const [activeButton, setActiveButton] = useState("dashboard");
 
-    const [dashboard, setDashboard] = useState(true);
-    const [card, setCard] = useState(false);
-    const [refresh, setRefresh] = useState(false);
-    const [settings, setSettings] = useState(false);
+    const buttonsData = [
+        { icon: "k-icon-dashboard", key: "dashboard" },
+        { icon: "k-icon-card", key: "card" },
+        { icon: "k-icon-refresh", key: "refresh" },
+        { icon: "k-icon-settings", key: "settings" },
+    ];
 
-    return(
+    return (
         <div className="nav-menu">
             <ButtonShowTransactions />
             <div className="nav-menu-icons">
-                <NavButton 
-                    icon="k-icon-dashboard" 
-                    class={dashboard ? "nav-button-active" : "nav-button-inactive"}
-                    onclick={() => {
-                        setDashboard(true)
-                        setCard(false)
-                        setRefresh(false)
-                        setSettings(false)
-                    }}
+                {buttonsData.map((button) => (
+                    <NavButton
+                        key={button.key}
+                        icon={button.icon}
+                        class={activeButton === button.key ? "nav-button-active" : "nav-button-inactive"}
+                        onclick={() => setActiveButton(button.key)}
                     />
-                <NavButton 
-                    icon="k-icon-card"
-                    class={card ? "nav-button-active" : "nav-button-inactive"}
-                    onclick={() => {
-                        setDashboard(false)
-                        setCard(true)
-                        setRefresh(false)
-                        setSettings(false)
-                    }}
-                    />
-                <NavButton 
-                    icon="k-icon-refresh" 
-                    class={refresh ? "nav-button-active" : "nav-button-inactive"}
-                    onclick={() => {
-                        setDashboard(false)
-                        setCard(false)
-                        setRefresh(true)
-                        setSettings(false)
-                    }}
-                    />
-                <NavButton 
-                    icon="k-icon-settings"
-                    class={settings ? "nav-button-active" : "nav-button-inactive"}
-                    onclick={() => {
-                        setDashboard(false)
-                        setCard(false)
-                        setRefresh(false)
-                        setSettings(true)
-                    }}
-                    />
-        </div>
+                ))}
+            </div>
         </div>
     );
 }
